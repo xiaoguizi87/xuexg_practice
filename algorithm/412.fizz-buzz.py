631b2063240f150fb7486103fd32d9ce830fe305
xiaoguizi87 <xiaoguizi87@163.com>
2019-03-17 05:43:20 +0800

    ac leetcode 412.fizz-buzz.py

---
+++ algorithm/412.fizz-buzz.py
@@ -0,0 +1,61 @@
+#
+# @lc app=leetcode id=412 lang=python3
+#
+# [412] Fizz Buzz
+#
+# https://leetcode.com/problems/fizz-buzz/description/
+#
+# algorithms
+# Easy (58.98%)
+# Total Accepted:    186.5K
+# Total Submissions: 316.2K
+# Testcase Example:  '1'
+#
+# Write a program that outputs the string representation of numbers from 1 to
+# n.
+# 
+# But for multiples of three it should output “Fizz” instead of the number and
+# for the multiples of five output “Buzz”. For numbers which are multiples of
+# both three and five output “FizzBuzz”.
+# 
+# Example:
+# 
+# n = 15,
+# 
+# Return:
+# [
+# ⁠   "1",
+# ⁠   "2",
+# ⁠   "Fizz",
+# ⁠   "4",
+# ⁠   "Buzz",
+# ⁠   "Fizz",
+# ⁠   "7",
+# ⁠   "8",
+# ⁠   "Fizz",
+# ⁠   "Buzz",
+# ⁠   "11",
+# ⁠   "Fizz",
+# ⁠   "13",
+# ⁠   "14",
+# ⁠   "FizzBuzz"
+# ]
+# 
+# 
+#
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        res = []
+        for i in range(1, n+1):
+            t = ''
+            if i % 3 == 0:
+                t += 'Fizz'
+            if i % 5 == 0:
+                t += 'Buzz'
+            if t:
+                res.append(t)
+            else:
+                res.append(str(i))
+        return res
+
+
