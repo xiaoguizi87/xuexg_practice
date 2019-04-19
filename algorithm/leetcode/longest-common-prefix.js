0c9d15909e25adb1057b20bfed40d84d8acd1b09
薛晓桂 Xue Xiaogui <xiaoguizi87@163.com>
2019-04-19 13:27:59 +0800

    Create longest-common-prefix.js

---
+++ algorithm/leetcode/longest-common-prefix.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if (strs.length == 0) {
+        return ''
+    }
+    let res = ''
+    strs.sort((x, y) => x.length - y.length)
+    for (let i = 0; i < strs[0].length; i++) {
+        for (let j = 0; j < strs.length -1; j++) {
+            if (strs[j][i] != strs[j + 1][i]) {
+                return res
+            }
+        }
+        res += strs[0][i]
+    }
+        
+    return res
+};
