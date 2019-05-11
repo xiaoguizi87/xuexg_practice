fd9fc27fdb663cc3489051900880f9ab6deec98d
薛晓桂 Xue Xiaogui <xiaoguizi87@163.com>
2019-05-11 20:56:51 +0800

    Update repeated-substring-pattern.js

--- algorithm/leetcode/repeated-substring-pattern.js
+++ algorithm/leetcode/repeated-substring-pattern.js
@@ -1,16 +1,7 @@
 /**
- * @param {number[]} rec1
- * @param {number[]} rec2
+ * @param {string} s
  * @return {boolean}
  */
-var isRectangleOverlap = function(rec1, rec2) {
-    let [x1, y1, x2, y2] = rec1;
-    let [xx1, yy1, xx2, yy2] = rec2;
-    if (x2 <= xx1 || x1 >= xx2) {
-        return false    
-    }
-    if (y2 <= yy1 || y1 >= yy2) {
-        return false
-    }
-    return true
+var repeatedSubstringPattern = function(s) {
+    return (s + s).substring(1, s.length * 2 - 1).indexOf(s) !== -1
 };
