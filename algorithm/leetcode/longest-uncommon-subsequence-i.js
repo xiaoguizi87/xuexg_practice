eaad2cd2468d28eba8561a05e98c8e0cf6b1465a
薛晓桂 Xue Xiaogui <xiaoguizi87@163.com>
2019-06-20 12:51:05 +0800

    Create longest-uncommon-subsequence-i.js

---
+++ algorithm/leetcode/longest-uncommon-subsequence-i.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {number}
+ */
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {number}
+ */
+var findLUSlength = function(a, b) {
+    if (a === b) {
+        return -1
+    }
+    return (a.length > b.length) ? a.length : b.length
+};
