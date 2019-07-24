ea76a91124cb8c0b35d3939f4b19f3808eca7c25
薛晓桂 Xue Xiaogui <xiaoguizi87@qq.com>
2019-07-24 16:48:30 +0800

    Create contains-duplicate-ii.js

---
+++ algorithm/leetcode/contains-duplicate-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    let map = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] in map) {
+            if (i - map[nums[i]] <= k) {
+                return true
+            }
+        }
+        map[nums[i]] = i
+    }
+    return false
+};
