4921712bd479ff7653408f2f75801d93eb85398a
薛晓桂 Xue Xiaogui <xiaoguizi87@qq.com>
2019-09-04 22:19:54 +0800

    Create symmetric-tree.js

---
+++ algorithm/leetcode/symmetric-tree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    return isMirror(root, root)
+};
+
+var isMirror = function(p, q) {
+    if (!p && !q) {
+        return true
+    }
+    if (!p || !q) {
+        return false
+    }
+    if (p.val === q.val) {
+        return isMirror(p.left, q.right) && isMirror(p.right, q.left)
+    }
+    return false
+}
